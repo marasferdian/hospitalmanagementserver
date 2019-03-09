817b05abf501002d5759d47772ee421893db62cd
Vlad Rochian <vlad.rochian@gmail.com>
2019-03-09 12:43:07 +0200

    Add gradle project

---
+++ src/main/java/com/cmedresearch/officemaptool/OfficemaptoolApplication.java
@@ -0,0 +1,13 @@
+package com.cmedresearch.officemaptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficemaptoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficemaptoolApplication.class, args);
+	}
+
+}
