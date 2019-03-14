fe6be01c8d85f50c87603b8ed0fe3133e0dbb717
Vlad Rochian <vlad.rochian@gmail.com>
2019-03-14 17:06:50 +0200

    Make service transactional

--- src/main/java/com/cmedresearch/officemaptool/service/SeatService.java
+++ src/main/java/com/cmedresearch/officemaptool/service/SeatService.java
@@ -6,9 +6,11 @@ import org.apache.commons.collections4.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class SeatService {
   private SeatRepository seatRepository;
 
